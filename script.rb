c8ff53e7cbf9b412024a458fca2e93d6fc8a047e
bellom <bellomsean@gmail.com>
2019-04-05 14:03:11 +0100

    code can now work with both array and hash

--- script.rb
+++ script.rb
@@ -2,14 +2,14 @@ module Enumerable
 
     def my_each
       # your code here
-      for i in (0...self.length) do
+      for i in (0...self.size) do
         yield self[i]
       end
       self
     end
   
     def my_each_with_index
-      for i in (0...self.length) do
+      for i in (0...self.size) do
         yield(self[i], i)
       end
       self
