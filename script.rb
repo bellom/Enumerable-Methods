eae6c6d70a58b1c8d6ac582aa14c470e33ed3502
bellom <bellomsean@gmail.com>
2019-04-08 11:20:44 +0100

    methods now  working on both array and hash

--- script.rb
+++ script.rb
@@ -25,45 +25,87 @@ module Enumerable
   end
 
   def my_select
-    array = []
-    self.my_each do |i|
-      array << i if yield(i)
+    arr = []
+    for i in self
+      if yield(i)
+        arr << i
+      end
     end
-    array
+
+    arr
   end
 
-  def my_all
-    self.my_each do |i|
-      return false if not yield i 
+def my_all?
+  arr = self.to_a
+  false_counter = 0
+  i = 0
+  while i < self.size
+    break if false_counter > 0
+    if block_given?
+        if yield(arr[i]) == false
+          false_counter += 1
+        end
+    elsif arr[i].nil? or arr[i] == false
+        false_counter += 1
     end
-    true
+    i += 1
   end
-
-  def my_any
-    self.my_each do |i| 
-      return true if yield i 
-    end
-    false
+  false_counter == 0 ? true : false
   end 
 
-  def my_none 
-    self.my_each do |i|
-      return false if yield i 
+def my_any?
+  arr = self.to_a
+  true_counter = 0
+  i = 0
+  while i < self.size
+    break if true_counter > 0
+    if block_given?
+      if yield(arr[i]) == true
+        true_counter += 1
+      end
+    elsif arr[i] != nil and arr[i] != false
+      true_counter += 1
     end
-    true 
-  end 
+    i += 1
+  end
+  true_counter == 0 ? false : true
+end
 
-  def my_count
-    count = 0 
-    self.my_each {|i| count += 1}
+
+def my_none?
+        arr = self.to_a
+        true_counter = 0
+        i = 0
+        while i < self.size
+            break if true_counter > 0
+            if block_given?
+                if yield(arr[i]) == true
+                    true_counter += 1
+                end
+            elsif arr[i] != nil and arr[i] != false
+                true_counter += 1
+            end
+            i += 1
+        end
+        true_counter == 0 ? true : false
+    end 
+
+  def my_count(target=nil)
+    if !block_given? && target.nil?
+      return self.size
+    end
+
+    count = 0
+    self.my_each do |e|
+      if block_given? && yield(e)
+        count += 1
+      elsif e == target
+        count += 1
+      end
+    end
+
     count
-  end
-
-  # def my_map
-  #   array = []
-  #   self.my_each {|i| array << yield(i)}
-  #   array
-  # end 
+  end 
 
   def my_inject(start = self[0])
     pointer = start
@@ -72,23 +114,22 @@ module Enumerable
       pointer = yield(pointer, i)
     end
     pointer
-  end
-
-  #Modify my_map method to take a proc instead.
-  # def my_map(proc_map)
-  #   array = []
-  #   self.my_each {|i| array << proc_map.call(i)}
-  #   array
-  # end 
+  end 
 
   #Modify your #my_map method to take either a proc
-  def my_map(&proc_map)
-    array = []
-    self.my_each do |i|
-      array << yield(i) if !proc_map
-      array << proc_map.call(i)
-    end 
-    array
+  def my_map(proc = nil)
+    arr = self.to_a
+    new_arr = Array.new
+    i = 0
+    while i < self.size
+        if block_given?
+          new_arr << yield(arr[i])
+        else
+          new_arr << proc.call(arr[i])
+        end
+        i += 1
+    end
+    new_arr
   end 
   
 end
@@ -102,25 +143,25 @@ end
 multiply_els ([2,4,5])
   
   
-  #To test the methods:
-  array = [1,2,3,4,5]
-  array.my_each {|i| puts i}
+  # To test the methods:
+  # array = [1,2,3,4,5,6]
+  # array.my_each {|i| puts i}
   # array.my_each_with_index {|i,j| puts i.to_s + " " + j.to_s}
   # puts array.my_select {|i| i > 5}
   # puts array.select {|i| i > 5}
-  # puts array.my_all {|x| x < 5}
-  #puts array.my_none {|x| x < 5}
-  #puts array.my_any {|x| x < 3}
-  #puts array.my_count {|i| i > 2}
-  #puts ["A","B"].my_map {|x| x.upcase}
+  # puts array.my_all? {|x| x > 0}
+  # puts array.my_none {|x| x < 5}
+  # puts array.my_any {|x| x < 3}
+  # puts array.my_count {|i| i > 2}
+  # puts array.my_map {|x| x*2}
   
 
-h = { "a" => 100, "b" => 200, "c" => 300 }
-  h.my_each {|i| puts i}
-  h.my_each_with_index {|i,j| puts i.to_s + " " + j.to_s}
-  #puts h.select {|k,v| k > "a"}
-  #puts h.my_all {|k| k < 5}
-  #puts h.my_none {|x| x < 5}
-  #puts h.my_any {|x| x < 3}
-  #puts h.my_count {|i| i > 2}
-  #puts ["A","B"].my_map {|x| x.upcase}
+  # h = { "a" => 100, "b" => 200, "c" => 80 }
+  # h.my_each {|i| puts i}
+  # h.my_each_with_index {|i,j| puts i.to_s + " " + j.to_s}
+  # h.my_select {|k,v| k > "a"}
+  # puts h.my_count
+  # puts h.my_all? {|k, v| v < 10}
+  # puts h.my_none? {|k,v| v < 90}
+  # puts h.my_any? {|k,v| v < 90}
+  # h.my_map { |k,v| v*2 } 
